29058dd9f4ce28bb783b971cdccb76c215e7c3cc
Flávio Paixão <flavio_pc@outlook.com>
2022-05-04 16:58:57 -0300

    mapeamento jpa

--- src/main/java/com/dio/santander/banklineapi/model/Correntista.java
+++ src/main/java/com/dio/santander/banklineapi/model/Correntista.java
@@ -1,9 +1,22 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Correntista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String cpf;
+    @Column(length = 60)
     private String nome;
+
+    @Embedded
     private Conta conta;
 
     public Conta getConta() {
